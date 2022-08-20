b9ce5929c5fa3f47831c95da35f405d08ea37814
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-08-20 15:23:02 +0500

    initial

---
+++ src/models/ItemModel.js
@@ -0,0 +1,14 @@
+class ItemModel {
+	constcructor(brand, title, description,
+		descriptionFull, price, currency) {
+		
+		this.brand = brand;
+		this.title = title;
+		this.description = description;
+		this.descriptionFull = descriptionFull;
+		this.price = price;
+		this.currency = currency;
+	}
+}
+
+export default ItemModel;
